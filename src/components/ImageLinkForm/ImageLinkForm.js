6a69850336d4d811957978537d28f5a5508b8715
TechGenius-Karan <karanmhetar595@gmail.com>
2023-06-24 17:23:46 +0530

    adding starting files

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ImageLinkForm.css';
+import ParticlesBg from 'particles-bg';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className='f3'>
+        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+      </p>
+      <div className='center'>
+      <ParticlesBg type="cobweb" color='#ffffff' num={250} bg={true} />
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            onClick={onButtonSubmit}
+          >Detect</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
